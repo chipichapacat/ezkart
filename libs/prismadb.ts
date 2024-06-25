337a85d228c6af96f5e853c2ab20c26426dbc6ab
RaashiK16 <126188705+RaashiK16@users.noreply.github.com>
2024-06-26 04:26:40 +0530

    update prismadb.ts

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -1,25 +1,50 @@
+// import { PrismaClient } from "@prisma/client";
+
+// declare global{
+//     var prisma: PrismaClient 
+// }
+
+// // const client = globalThis.prisma || new PrismaClient()
+
+// // if(process.env.NODE_ENV  != "production")
+// //     globalThis.prisma = client
+
+
+// // export default client;
+
+// if(process.env.NODE_ENV === "production"){
+//     prisma = new PrismaClient();
+// }
+// else{
+//     if(!global.prisma){
+//         global.prisma = new PrismaClient();
+//     }
+//     prisma = global.prisma;
+// }
+
+// export default prisma;
+
+
 import { PrismaClient } from "@prisma/client";
 
-declare global{
-    var prisma: PrismaClient 
+declare global {
+    var prisma: PrismaClient;
 }
 
-// const client = globalThis.prisma || new PrismaClient()
+let prisma: PrismaClient;
 
-// if(process.env.NODE_ENV  != "production")
-//     globalThis.prisma = client
-
-
-// export default client;
-
-if(process.env.NODE_ENV === "production"){
-    prisma = new PrismaClient();
-}
-else{
-    if(!global.prisma){
-        global.prisma = new PrismaClient();
+// Initialize prisma if not already initialized
+if (process.env.NODE_ENV !== "production") {
+    prisma = globalThis.prisma || new PrismaClient();
+    if (process.env.NODE_ENV !== "test") {
+        globalThis.prisma = prisma;
     }
-    prisma = global.prisma;
+} else {
+    // In production, ensure a single instance of PrismaClient
+    if (!globalThis.prisma) {
+        globalThis.prisma = new PrismaClient();
+    }
+    prisma = globalThis.prisma;
 }
 
 export default prisma;

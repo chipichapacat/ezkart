8026fcaa62913bb953fd906196fa263aa85475e9
RaashiK16 <126188705+RaashiK16@users.noreply.github.com>
2024-06-26 18:47:48 +0530

    updated order/route.ts

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -2,6 +2,8 @@ import prisma from "@/libs/prismadb"
 import { NextResponse } from "next/server";
 import { getCurrentUser } from "@/actions/getCurrentUser";
 
+export const runtime = "nodejs"
+export const dynamic = "force-dynamic"
 
 export async function PUT(request:Request){
     const currentUser = await getCurrentUser();

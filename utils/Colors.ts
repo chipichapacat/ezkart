f7afe729e6340f897984169fd907090150f04abd
RaashiK16 <126188705+RaashiK16@users.noreply.github.com>
2024-06-27 20:05:47 +0530

    update colors.ts

--- utils/Colors.ts
+++ utils/Colors.ts
@@ -1,12 +1,22 @@
 export const colors = [
     {
         color: "Black",
-        colorCode:"#FFD700",
+        colorCode:"#000000",
         image: null
     },
     {
         color: "White",
         colorCode:"#ffffff",
+        image: null
+    },
+    {
+        color: "Midnight Blue",
+        colorCode:"#405268",
+        image: null
+    },
+    {
+        color: "Gray",
+        colorCode:"#808080",
         image: null
     },
     {
@@ -20,14 +30,74 @@ export const colors = [
         image: null
     },
     {
-        color: "Gray",
-        colorCode:"#808080",
-        image: null
+        color:"Starlight",
+        colorCode: "#e1dad0",
+        image:null
     },
     {
         color: "Red",
         colorCode:"#ff0000",
         image: null
+    },
+    {
+        color: "Phoenix Red",
+        colorCode:"c98e80",
+        image:null
+    },
+    {
+        color:"Sea Mist",
+        colorCode:"#c6dcc5",
+        image:null
+    },
+    {
+        color:"Ice Blue",
+        colorCode:"#7aa1c2",
+        image:null
+    },
+    {
+        color:"Pastel Blue",
+        colorCode:"c6e0f8",
+        image:null
+    },
+    {
+        color:"Pastel Purple",
+        colorCode:"#b4a7d6",
+        image:null
+    },
+    {
+        color:"Pastel Pink",
+        colorCode:"#f4cccc",
+        image:null
+    },
+    {
+        color:"Satin Blue",
+        colorCode:"#16537e",
+        image:null
+    },
+    {
+        color:"Mint",
+        colorCode:"#cbe7be",
+        image:null
+    },
+    {
+        color:"Smoky Teal",
+        colorCode:"#9ac1c7",
+        image:null
+    },
+    {
+        color:"Pastel Yellow",
+        colorCode:"#ffe599",
+        image:null
+    },
+    { 
+        color:"Crystal Blue",
+        colorCode:"#e7f2fb",
+        image:null
+    },
+    {
+        color:"Sea Blue",
+        colorCode:"#1d5c87",
+        image:null
     },
     {
         color: "Gold",
